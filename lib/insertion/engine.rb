8a3d385ee7357f280afb3d3df6b5221aad72586c
Joel Moss <joel@developwithstyle.com>
2026-01-05 08:50:54 +0000

    fix: make Insertion a Rails engine and remove useless test

--- lib/insertion/engine.rb
+++ lib/insertion/engine.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'rails/engine'
+
 module Insertion
   class Railtie < Rails::Engine
   end
--- test/test_insertion.rb
+++ test/test_insertion.rb
@@ -6,8 +6,4 @@ class TestInsertion < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Insertion::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
